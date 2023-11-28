153ac48714a2f3b8ef790610f6b8ee4841ace925
rgfgr <88767865+rgfgr@users.noreply.github.com>
2023-11-28 09:57:41 +0100

    thing

--- Entities/Customer.cs
+++ Entities/Customer.cs
@@ -3,8 +3,7 @@
     public class Customer
     {
         public int CustomerId { get; set; }
-        public string CustomerEmail { get; set; }
-        public string CustomerPhone { get; set; }
+        public string Name { get; set; }
         public List<Booking>? Bookings { get; set; }
     }
 }
